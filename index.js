8a7bd9f5c9fcbe3bc4f8ed1e1df56663e8ac3694
nader <nader90akhgari@gmail.com>
2021-05-30 01:33:05 +0100

    create a server which serves a static html file

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-import express from 'express'
+import express from "express";
+import http from "http";
+const app = express();
+const server = http.createServer(app);
+import { Server } from "socket.io";
+const io = new Server(server);
+const port = 3001;
+
+server.listen(port, () => {
+  console.log(`server is listening to the port: ${port}`);
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
