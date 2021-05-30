663566a21cb3fe8f0416e64b2a19f08ae4764a42
nader <nader90akhgari@gmail.com>
2021-05-30 02:12:16 +0100

    created a connection by socket.io to emit a message

--- index.js
+++ index.js
@@ -13,3 +13,9 @@ server.listen(port, () => {
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
+
+io.on("connection", (socket) => {
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
+});
